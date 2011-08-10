0f7ce4abe91f0ec426a6a25494ba59f7bef151af
Átila Camurça Alves <camurca.home@gmail.com>
2011-08-10 10:54:29 -0300

    test zenity version. only works with zenity 2.32 or greater.

--- color-grab.sh
+++ color-grab.sh
@@ -18,7 +18,22 @@
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 
-COLOR=$(zenity --color-selection --title "Color-grab: Color Selection" --show-palette)
+# test if zenity exists
+ZENITY="/usr/bin/zenity"
+command -v $ZENITY >/dev/null && continue || { echo "zenity not found. Try install: sudo apt-get install zenity"; exit 1; }
+
+# test version greater equal 2.32
+VERSION=`$ZENITY --version | cut -c 1-4`
+REQUIRED_VERSION=2.32
+if [ "$(echo $VERSION '<' $REQUIRED_VERSION | bc)" -eq 1 ]; then
+	$ZENITY --error --text "Color-grab needs zenity 2.32 or greater." --title "Color-grab: Error"
+	echo "Color-grab needs zenity 2.32 or greater."; exit 1
+fi
+
+COLOR=$($ZENITY --color-selection --title "Color-grab: Color Selection" --show-palette)
+if [ -z $COLOR ]; then
+	echo "Cancel was pressed. Good-bye."; exit 1
+fi
 RGB="$(echo $COLOR | cut -c 2-3)$(echo $COLOR | cut -c 6-7)$(echo $COLOR | cut -c 10-11)"
-zenity --info --title "Color-grab" --text "Selected color: #$(echo $RGB | awk '{print toupper($0)}')"
+$ZENITY --info --title "Color-grab" --text "Selected color: #$(echo $RGB | awk '{print toupper($0)}')"
 exit 0
